f749e1848aa6782b8ab4f2644505779333a6c80d
Samo <paulsamo1234@gmail.com>
2021-07-03 18:05:19 +0530

    first commit

---
+++ data/blogs.js
@@ -0,0 +1,24 @@
+blogs= [
+        {
+        title:"How to get started with python",
+        content:"This is Python",
+        slug:"learn-python"
+    },
+      {
+        title:"How to get started with JS",
+        content:"This is JavaScript",
+        slug:"learn-JavaScript"
+    },
+      {
+        title:"How to get started with Flask",
+        content:"This is Flask",
+        slug:"learn-Flask"
+    },
+      {
+        title:"How to get started with CSS",
+        content:"This is CSS",
+        slug:"learn-CSS"
+    },
+    ]
+
+module.exports = blogs;  
---
+++ routes/blog.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router();
+const path = require('path')
+const blogs = require('../data/blogs')
+
+router.get("/", (req, res) => {
+    // res.sendFile(path.join(__dirname,'../templates/index.html'))
+    res.render('home')
+});
+router.get("/blog", (req, res) => {
+    // blogs.forEach(e => {
+    //     console.log(e.title)
+    // });
+    // res.sendFile(path.join(__dirname,'../templates/blogIndex.html'))
+
+    res.render('blogHome',{
+        blogs:blogs
+    })
+});
+router.get("/blogpost/:slug", (req, res) => {
+    myBlog=blogs.filter((e)=>{
+        return e.slug==req.params.slug
+    })
+    // console.log(myBlog)
+
+    // res.sendFile(path.join(__dirname,'../templates/blogPage.html'))
+  res.render('blogPage',{
+        title:myBlog[0].title,
+        content:myBlog[0].content
+    })
+});
+
+
+
+module.exports = router;
